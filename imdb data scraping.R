5fe68700260fdfacc1fc8ce5d4f65f26e6092188
Aytantabriz <51262587+Aytantabriz@users.noreply.github.com>
2022-12-21 11:13:05 +0400

    Add files via upload

---
+++ imdb data scraping.R
@@ -0,0 +1,122 @@
+#Loading the rvest package
+library('rvest')
+
+#Specifying the url for desired website to be scraped
+url <- 'http://www.imdb.com/search/title?count=100&release_date=2016,2016&title_type=feature'
+
+#Reading the HTML code from the website
+webpage <- read_html(url)
+
+#====================================Ranking===================================================================
+#Using CSS selectors to scrape the rankings section
+rank_data_html <- html_nodes(webpage,'.text-primary')
+
+#Converting the ranking data to text
+rank_data <- html_text(rank_data_html)
+
+#Let's have a look at the rankings
+head(rank_data)
+
+#Data-Preprocessing: Converting rankings to numerical
+rank_data<-as.numeric(rank_data)
+
+#Let's have another look at the rankings
+head(rank_data)
+
+#====================================Title===================================================================
+#Using CSS selectors to scrape the title section
+title_data_html <- html_nodes(webpage,'.lister-item-header a')
+
+#Converting the title data to text
+title_data <- html_text(title_data_html)
+
+#Let's have a look at the title
+head(title_data)
+
+#====================================Description===================================================================
+#Using CSS selectors to scrape the description section
+description_data_html <- html_nodes(webpage,'.ratings-bar+ .text-muted')
+
+#Converting the description data to text
+description_data <- html_text(description_data_html)
+
+#Let's have a look at the description data
+head(description_data)
+
+#Data-Preprocessing: removing '\n'
+description_data<-gsub("\n","",description_data)
+
+#Let's have another look at the description data 
+head(description_data)
+
+#====================================Movie runtime===================================================================
+#Using CSS selectors to scrape the Movie runtime section
+runtime_data_html <- html_nodes(webpage,'.text-muted .runtime')
+
+#Converting the runtime data to text
+runtime_data <- html_text(runtime_data_html)
+
+#Let's have a look at the runtime
+head(runtime_data)
+
+#Data-Preprocessing: removing mins and converting it to numerical
+
+runtime_data<-gsub(" min","",runtime_data)
+runtime_data<-as.numeric(runtime_data)
+
+#Let's have another look at the runtime data
+head(runtime_data)
+
+#====================================Genre===================================================================
+#Using CSS selectors to scrape the Movie genre section
+genre_data_html <- html_nodes(webpage,'.genre')
+
+#Converting the genre data to text
+genre_data <- html_text(genre_data_html)
+
+#Let's have a look at the runtime
+head(genre_data)
+
+#Data-Preprocessing: removing \n
+genre_data<-gsub("\n","",genre_data)
+
+#Data-Preprocessing: removing excess spaces
+genre_data<-gsub(" ","",genre_data)
+
+#taking only the first genre of each movie
+genre_data<-gsub(",.*","",genre_data)
+
+#Convering each genre from text to factor
+genre_data<-as.factor(genre_data)
+
+#Let's have another look at the genre data
+head(genre_data)
+
+#====================================IMDB===================================================================
+#Using CSS selectors to scrape the IMDB rating section
+rating_data_html <- html_nodes(webpage,'.ratings-imdb-rating strong')
+
+#Converting the ratings data to text
+rating_data <- html_text(rating_data_html)
+
+#Let's have a look at the ratings
+head(rating_data)
+
+#Data-Preprocessing: converting ratings to numerical
+rating_data<-as.numeric(rating_data)
+
+#Let's have another look at the ratings data
+head(rating_data)
+
+#====================================Combining===================================================================
+#Combining all the lists to form a data frame
+movies_df<-data.frame(Rank = rank_data, Title = title_data,
+                      
+                      Description = description_data, Runtime = runtime_data,
+                      
+                      Genre = genre_data, Rating = rating_data)
+
+#Structure of the data frame
+
+str(movies_df)
+glimpse(movies_df)
